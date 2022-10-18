ace21fed248c98aa78283ee4fc130f1d8a9e86b3
Alejandra Guzman <77361246+guzmanalejandra@users.noreply.github.com>
2022-10-18 00:18:07 -0600

    Update Raytracer.py

--- Raytracer.py
+++ Raytracer.py
@@ -7,27 +7,32 @@ from lights import *
 width = 512
 height = 512
 
+rtx = Raytracer(width, height)
+# env map
+
+rtx.envMap = Texture("oculus.bmp")
+
+
 # Materiales
 
 brick = Material(diffuse = (0.8, 0.3, 0.3), spec = 16)
 stone = Material(diffuse = (0.4, 0.4, 0.4), spec = 8)
 grass = Material(diffuse = (0.3, 1.0, 0.3), spec = 64)
-
-
+blockstone = Material(spec = 64, texture = Texture("blockstone.bmp"), matType= REFLECTIVE)
 marble = Material(spec = 64, texture = Texture("marble.bmp"), matType= REFLECTIVE)
-
 mirror = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, matType = REFLECTIVE)
 glass = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, ior = 1.5, matType = TRANSPARENT)
+cube = Material(spec = 64, texture = Texture("cube.bmp"))
 
-rtx = Raytracer(width, height)
 
-rtx.envMap = Texture("parkingLot.bmp")
+
+# lights
 
 rtx.lights.append( AmbientLight(intensity = 0.1 ))
-#rtx.lights.append( DirectionalLight(direction = (0,0,-1), intensity = 0.5 ))
+rtx.lights.append( DirectionalLight(direction = (-1,-1,-1), intensity = 0.5 ))
 rtx.lights.append( PointLight(point = (-1,-1,0) ))
 
-
+#shapes
 rtx.scene.append( Plane(position = (0,-10,0), normal = (0,1,0), material = brick ))
 rtx.scene.append( Plane(position = (0,10,0), normal = (0,-1,0), material = brick ))
 rtx.scene.append( Plane(position = (-10,0,0), normal = (1,0,0), material = stone ))
@@ -39,6 +44,5 @@ rtx.scene.append( Disk(position = (0,-3,-7), radius = 2, normal = (0,1,0), mater
 rtx.scene.append( AABB(position = (-2,1,-10), size = (2,2,2), material = glass))
 rtx.scene.append( AABB(position = (2,1,-10), size = (2,2,2), material = marble))
 
-rtx.glRender()
 
-rtx.glFinish("output.bmp")
+rtx.glRender()
